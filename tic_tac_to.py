6f6122fbda1d24c44b13fe1787943ca7776897e0
채민수 <chaeminsu@chaeminsuui-MacBookPro.local>
2022-10-06 21:30:09 +0900

    Implementation

--- tic_tac_to.py
+++ tic_tac_to.py
@@ -1,25 +1,31 @@
 # 7682
 def win_game(data,ox):
+    cnt = 0
     if data[0] == ox and data[0] == data[1] == data[2]: # 첫번째 가로줄 완성
-        return True
+        cnt+=1 # return True
     elif data[3] == ox and data[3] == data[4] == data[5]: # 두첫번째 가로줄 완성
-        return True
+        cnt+=1 # return True
     elif data[6] == ox and data[6] == data[7] == data[8]: # 세번째 가로줄 완성
-        return True
+        cnt+=1 # return True
 
     elif data[0] == ox and data[0] == data[3] == data[6]: # 첫번째 세로줄 완성
-        return True
+        cnt+=1 # return True
     elif data[1] == ox and data[1] == data[4] == data[7]: # 두첫번째 세로줄 완성
-        return True
+        cnt+=1 # return True
     elif data[2] == ox and data[2] == data[5] == data[8]: # 세번째 세로줄 완성
-        return True
+        cnt+=1 # return True
 
     elif data[0] == ox and data[0] == data[4] == data[8]: # 오른쪽 아래 대각선 완성
-        return True
+        cnt+=1 # return True
     elif data[6] == ox and data[6] == data[4] == data[2]: # 왼쪽 아래 대각선 완성
+        cnt+=1 # return True
+
+    if cnt == 1:
         return True
-    
-    else: return False # 못이김
+    else:
+        return False
+
+    # else: return False # 못이김
 
 def checking(data):
     x_num = data.count('X')
