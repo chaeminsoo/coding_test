64584198dc126649aa8b6e2b20a3a92d68ce3115
chaeminsu <chaemiinsu@gmail.com>
2022-12-02 02:09:06 +0900

    Greedy

---
+++ road_camera.py
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=python3
+def solution(routes):
+    answer = 0
+    num_car = len(routes)
+    routes.sort(key = lambda x : x[1])
+    camera_spot = -30000
+    
+    for car_in, car_out in routes:
+        if camera_spot < car_in:
+            camera_spot = car_out
+            answer+=1
+    return answer
