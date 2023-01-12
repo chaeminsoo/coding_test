602ae0c6c6dcbc41f9e16438e5bff7a7b9010799
chaeminsu <chaemiinsu@gmail.com>
2023-01-13 03:22:48 +0900

    DP

--- color_wheel.py
+++ color_wheel.py
@@ -9,12 +9,9 @@ for i in range(n+1):
 
 for i in range(2,n+1):
     for j in range(2,k+1):
-        # if i//2 < j:
-        #     continue
         if i == n:
             dp[i][j] = dp[i-3][j-1] + dp[i-1][j]
         else:
             dp[i][j] = dp[i-2][j-1] + dp[i-1][j]
-
-for i in dp:
-    print(i)
+            
+print(dp[n][k]%1000000003)
