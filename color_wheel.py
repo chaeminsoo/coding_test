adb851775923babfd208ff2bc120a8923b2b5699
chaeminsu <chaemiinsu@gmail.com>
2023-01-13 03:22:10 +0900

    DP

---
+++ color_wheel.py
@@ -0,0 +1,20 @@
+# 2482
+n = int(input())
+k = int(input())
+dp = [[0]*(k+1) for _ in range(n+1)] # dp[n][k] = ans
+
+
+for i in range(n+1):
+    dp[i][1] = i
+
+for i in range(2,n+1):
+    for j in range(2,k+1):
+        # if i//2 < j:
+        #     continue
+        if i == n:
+            dp[i][j] = dp[i-3][j-1] + dp[i-1][j]
+        else:
+            dp[i][j] = dp[i-2][j-1] + dp[i-1][j]
+
+for i in dp:
+    print(i)
