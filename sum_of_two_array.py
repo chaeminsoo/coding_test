3a74a1a807a40b40f0a22d7bf42fbbcfa6e65907
chaeminsu <chaemiinsu@gmail.com>
2023-07-16 21:15:59 +0900

    array_sum

--- sum_of_two_array.py
+++ sum_of_two_array.py
@@ -1,35 +1,22 @@
 # 2143
-import sys
-input = sys.stdin.readline
+from collections import defaultdict
 
 t = int(input())
+
 n = int(input())
-a = list(map(int,input().split()))
+A = list(map(int,input().split()))
 m = int(input())
-b = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+sums = defaultdict(int)
+
+for i in range(n):
+    for j in range(i+1,n+1):
+        sums[sum(A[i:j])] += 1
 
 ans = 0
-dict_a = {}
-for i in range(n):
-    for j in range(1,n):
-        ref = sum(a[i:i+j])
-        try:
-            dict_a[ref] += 1
-        except KeyError:
-            dict_a[ref] = 1
-
-checks = {}
 for i in range(m):
-    for j in range(1,m):
-        ref = sum(b[i:i+j])
-        try:
-            checks[ref]
-            continue
-        except:
-            checks[ref] = True
-        ref2 = t -ref
-        try:
-            ans += dict_a[ref2]
-        except KeyError:
-            pass
-print(ans)     
+    for j in range(i+1,m+1):
+        ans += sums[t-sum(B[i:j])]
+
+print(ans)
